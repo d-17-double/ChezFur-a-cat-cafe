e60cbdc1501542aee5a6fe5aed86f2a1cdae838d
d-17-double <lynch.d@northeastern.edu>
2024-11-22 22:06:27 -0500

    Add files via upload

---
+++ scripts/Welcome.js
@@ -0,0 +1,11 @@
+
+function showWelcomeMessage() {
+    alert("Welcome pets of all ages, sizes and skills");
+}
+
+
+window.onload = function() {
+    showWelcomeMessage();
+};
+
+// This function was created to show time and date as a pop up message 
---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+
+
+const d = new Date(); 
+document.getElementById("demo").innerHTML = d; 
+
+//Created this function to show time and date
